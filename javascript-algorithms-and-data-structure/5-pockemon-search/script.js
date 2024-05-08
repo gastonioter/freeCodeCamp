b96c288d42f200434d9244fd180f1521c82bb846
gastonioter <iotting.castro10@gmail.com>
2024-05-08 00:22:25 -0300

    pokemon search app

---
+++ javascript-algorithms-and-data-structure/5-pockemon-search/script.js
@@ -0,0 +1,6 @@
+const pokemonsAPI = "https://pokeapi-proxy.freecodecamp.rocks/api/pokemon";
+// https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/{name-or-id}
+
+/* Note: Pokémon names should be in lowercase, have special characters removed, 
+and be dash separated. Also, if the Pokémon has either ♀ or ♂ as part of its 
+name, the format is {name-f} or {name-m}, respectively. */
